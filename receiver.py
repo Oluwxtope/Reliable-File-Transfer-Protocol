e227f2aad0d28351b69968abe673e4da067bdfd5
Emmanuel <oluwatopealofe@gmail.com>
2022-03-30 18:52:03 -0400

    sender and receiver works, yet to implement drop probability, countdown timer for acks, and buffer. updated gitignore to ignore test files

--- receiver.py
+++ receiver.py
@@ -1,10 +1,52 @@
 from socket import *
+from random import *
 
-# handle program startup, request inputs from user
-info = input("Enter the UDP port number to receive data, drop probability, and name of the file received data will be written: ")
+# request input from user
+info = input("Enter the UDP port number to receive packet, drop probability, and name of the file received packet will be written: ")
 info = info.split(" ")
-host_address = str(info[0])
-send_port = int(info[1])
-receive_port = int(info[2])
-time_out = int(info[3])
-file_name = str(info[4])
+receive_port = int(info[0]) # first input is port to receive packet
+drop_probability = float(info[1]) # second input is probability of packet being dropped
+file_name = str(info[2]) # third input is name of file to write
+file = open(file_name, 'w')
+
+# create udp socket, bind it to receive port
+receiver_socket = socket(AF_INET, SOCK_DGRAM)
+receiver_socket.bind(("", receive_port))
+
+# receive packets and write to log file arrival.log
+# drop packets based on drop probability and write to log file drop.log
+# write acked packets to new file
+arrival = open("arrival.log", 'w')
+dropped = open("drop.log", 'w')
+last_seqnum = -1
+while True:
+    packet, sender_address = receiver_socket.recvfrom(2048)
+    packet = packet.decode().split(" ")
+    ack = int(packet[0])
+    seqnum = int(packet[1])
+    length = int(packet[2])
+    arrival.write(str(seqnum) + "\n") # record seqnum to arrival.log
+    if ack == 1:
+        text = str(" ".join(packet[3:]))
+        if False: #random() < drop_probability / 100:
+            #dropped.write(str(seqnum) + "\n")
+            continue
+        elif seqnum == last_seqnum:
+            continue
+        elif seqnum - 1 == last_seqnum:
+            file.write(text)
+            packet = "0" + " " + str(seqnum) + " " + "0"
+            receiver_socket.sendto(packet.encode(), sender_address)
+            last_seqnum = seqnum
+            continue
+    elif ack == 2:
+        packet = "2" + " " + str(seqnum) + " " + "0"
+        receiver_socket.sendto(packet.encode(), sender_address)
+        break
+
+arrival.close()
+dropped.close()
+file.close()
+receiver_socket.close()
+
+# 11000 0.5 new.txt
--- sender.py
+++ sender.py
@@ -1,37 +1,60 @@
 from socket import *
 
-# handle program startup, request inputs from user
-info = input("Enter the host address of the receiver, UDP port number to send data to the receiver, UDP port number to receive ACKs from the receiver, timeout interval (milliseconds), and name of the file: ")
+# request input from user
+info = input("Enter the host address of the receiver, port number to used to send data, port number to receive ACKs from the receiver, timeout interval (milliseconds), and name of the file: ")
 info = info.split(" ")
-host_address = str(info[0])
-send_port = int(info[1])
-receive_port = int(info[2])
-time_out = int(info[3])
-file_name = str(info[4])
+host_address = str(info[0]) # first input is host address
+receive_port = int(info[1]) # second input is port for receiver to use
+send_port = int(info[2]) # third input is port for sender to send data/receive acks
+time_out = int(info[3]) # fourth input is timeout interval in milliseconds
+file_name = str(info[4]) # fifth input is name of file w extension
 
-# open file, read contents, store in data
+# open file, read contents, store in data string
 file = open(file_name, 'r')
 data = file.read()
 file.close()
 
-# create udp socket
-receiver_socket = socket(AF_INET, SOCK_DGRAM)
-receiver_socket.bind(("", receive_port))
+# create udp socket, bind send port to it
+sender_socket = socket(AF_INET, SOCK_DGRAM)
+sender_socket.bind(("", send_port))
 
-# send packets
-seqnum = 0
+# send packets and receive acks from receiver
+# write seqnum to log file seqnum.log
+# write ack to log file ack.log
 seqnum_file = open("seqnum.log", 'w')
-while len(data) > 0:
-    packet_data = data[0:500]
-    try:
-        data = data[500:]
-    except:
-        data = ""
-    packet = "1" + " " + str(seqnum) + " " + str(len(packet_data)) + str(packet_data)
-    receiver_socket.sendto(packet.encode(), (host_address, receive_port))
-    seqnum_file.write(str(seqnum))
-    seqnum += 1
-
-# receive acks
 ack_file = open("ack.log", 'w')
+seqnum = 0 # start seqnum at 0
+while len(data) > 0: # run loop for n/500^k > 0 times
+    file_data = data[0:500]
+    packet = "1" + " " + str(seqnum) + " " + str(len(file_data)) + " " + str(file_data)
+    sender_socket.sendto(packet.encode(), (host_address, receive_port))
+    seqnum_file.write(str(seqnum) + "\n") # write each seqnum sent to log
+    receive_data, receiver_address = sender_socket.recvfrom(2048)
+    receive_data = receive_data.decode().split(" ")
+    ack = int(receive_data[0])
+    new_seqnum = int(receive_data[1])
+    ack_file.write(str(new_seqnum) + "\n") # record ack to ack.log
+    if seqnum != new_seqnum:
+        continue
+    else:
+        seqnum += 1
+        try:
+            data = data[500:]
+        except:
+            data = ""
 
+packet = "2" + " " + str(seqnum) + " " + "0"
+sender_socket.sendto(packet.encode(), (host_address, receive_port))
+seqnum_file.write(str(seqnum) + "\n") # write each seqnum sent to log
+receive_data, receiver_address = sender_socket.recvfrom(2048)
+receive_data = receive_data.decode().split(" ")
+ack = int(receive_data[0])
+new_seqnum = int(receive_data[1])
+ack_file.write(str(new_seqnum) + "\n")
+
+if ack == 2:
+    seqnum_file.close()
+    ack_file.close()
+    sender_socket.close()
+
+# localhost 11000 12000 50 test.txt
